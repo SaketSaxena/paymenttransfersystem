dd6c8b5cbf74f3a6338216cd3577ee494d157595
saketsaxena9 <Cr!m!n@l0>
2022-10-13 10:18:22 +0200

    Add Spring Boot Initial setup

---
+++ src/test/java/com/saketsaxena/paymenttransfersystem/PaymentTransferSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.saketsaxena.paymenttransfersystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentTransferSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
