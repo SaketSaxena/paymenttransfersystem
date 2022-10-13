faca2200087d6f5cdb329d0777be135538ae5c15
saketsaxena9 <Cr!m!n@l0>
2022-10-13 12:35:21 +0200

    Refactor a bit to get account balance

--- src/main/java/com/saketsaxena/paymenttransfersystem/helper/AccountStore.java
+++ src/main/java/com/saketsaxena/paymenttransfersystem/helper/AccountStore.java
@@ -1,6 +1,7 @@
 package com.saketsaxena.paymenttransfersystem.helper;
 
 import com.saketsaxena.paymenttransfersystem.DTOs.AccountBalance;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
@@ -10,18 +11,22 @@ import java.util.List;
  * @author Saket Saxena
  * @since 1.0
  */
+@Service
 public class AccountStore {
 
     /** Represents the list of all the account balances.
      */
-    private static final List<AccountBalance> accountBalances = new ArrayList<>();
+    private final List<AccountBalance> accountBalances = new ArrayList<>();
+
+    public AccountStore() {
+        accountBalances.add(new AccountBalance(111, new BigDecimal("100.10"), "GBP"));
+        accountBalances.add(new AccountBalance(222, new BigDecimal("324.45"), "GBP"));
+    }
 
     /** Get all account balance .
      * @return A List representing the AccountBalance of all the users.
      */
-    public static List<AccountBalance> getAccountBalances(){
-        accountBalances.add(new AccountBalance(111, new BigDecimal("100.10"), "GBP"));
-        accountBalances.add(new AccountBalance(222, new BigDecimal("324.45"), "GBP"));
+    public List<AccountBalance> getAccountBalances() {
         return accountBalances;
     }
 }
