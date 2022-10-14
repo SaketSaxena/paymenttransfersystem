178317a04a912c52da7ebbb08cfd4ec73e158e7a
saketsaxena9 <Cr!m!n@l0>
2022-10-14 22:06:28 +0200

    Add Json property to PaymentTransfer

--- src/main/java/com/saketsaxena/paymenttransfersystem/DTOs/PaymentTransfer.java
+++ src/main/java/com/saketsaxena/paymenttransfersystem/DTOs/PaymentTransfer.java
@@ -1,5 +1,7 @@
 package com.saketsaxena.paymenttransfersystem.DTOs;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import java.math.BigDecimal;
 
 /** Creates an account balance with the specified account id.
@@ -7,5 +9,7 @@ import java.math.BigDecimal;
  * @param receiverAccountId account id of the sender who is receiving fund
  * @param transferAmount amount need to transfer.
  */
-public record PaymentTransfer(int senderAccountId, int receiverAccountId, BigDecimal transferAmount) {
+public record PaymentTransfer(@JsonProperty("sender-account-id") int senderAccountId,
+                              @JsonProperty("receiver-account-id") int receiverAccountId,
+                              @JsonProperty("transfer-amount") BigDecimal transferAmount) {
 }
